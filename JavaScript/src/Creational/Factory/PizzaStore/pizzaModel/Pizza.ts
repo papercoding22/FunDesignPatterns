1b8cc6f2aca429f8c46f3aa03a254edd3f73d36a
Paper Coding <john.ly997@gmail.com>
2021-02-19 01:07:37 +0700

    Create model

---
+++ JavaScript/src/Creational/Factory/PizzaStore/pizzaModel/Pizza.ts
@@ -0,0 +1,35 @@
+class Pizza {
+  name: string;
+  dough: string;
+  sauce: string;
+  toppings: any[];
+  constructor({ name = "", dough = "", toppings = [], saurce = "" }) {
+    (this.name = name), (this.dough = dough), (this.toppings = toppings);
+    this.sauce = saurce;
+  }
+
+  prepare() {
+    console.log("Preparing " + this.name);
+    console.log("Tossing dough...");
+    console.log("Adding sauce");
+    console.log("Adding toppings");
+  }
+
+  bake() {
+    console.log("Baking for 25 minutes at 350");
+  }
+
+  cut() {
+    console.log("Cutting the pizza into diagonal slices");
+  }
+
+  box() {
+    console.log("Place pizza in official Pizza Store box");
+  }
+
+  getName() {
+    return this.name;
+  }
+}
+
+export default Pizza;
