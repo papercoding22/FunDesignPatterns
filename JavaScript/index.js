b75b23288b985fd6376dc5767e8a77aa67be70fb
Paper Coding <john.ly997@gmail.com>
2021-02-18 20:07:14 +0700

    Setup TypeScript

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,20 @@
-console.log("Hello, World");
+var menu = require("console-menu");
+menu(
+  [
+    { hotkey: "1", title: "One" },
+    { hotkey: "2", title: "Two", selected: true },
+    { hotkey: "3", title: "Three" },
+    { separator: true },
+    { hotkey: "?", title: "Help" },
+  ],
+  {
+    header: "Example menu",
+    border: true,
+  }
+).then((item) => {
+  if (item) {
+    console.log("You chose: " + JSON.stringify(item));
+  } else {
+    console.log("You cancelled the menu.");
+  }
+});
